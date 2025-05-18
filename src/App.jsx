0773907731bacef8138e8431c9b866f5c9055049
Nemoske <luandevfreitas7@gmail.com>
2025-05-18 01:43:34 -0300

    Add tasks

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import AddTasks from "./assets/components/AddTasks";
 import Tasks from "./assets/components/Tasks";
+import {v4} from 'uuid'
 
 function App() {
   const [tasks, setTasks] = useState([
@@ -25,13 +26,49 @@ function App() {
     },
   ]);
 
+  function onTaskClick(taskId) {
+    const newTasks = tasks.map((task) => {
+      if (task.id == taskId) {
+        return { ...task, isCompleted: !task.isCompleted };
+      }
+
+      return task;
+    });
+    setTasks(newTasks);
+  }
+
+  function onDeleteTaskClick(taskId) {
+    const newTasks = tasks.filter((task) => task.id != taskId);
+    setTasks(newTasks);
+  }
+
+  function onAddTaskSubmit(title, description){
+    const newTask = {
+      id: v4(),
+      title,
+      description,
+      isCompleted: false,
+    };
+    setTasks([...tasks, newTask])
+
+  }
+
   return (
     <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
-      <h1 className="text-3xl text-slate-100 font-bold text-center">
-        Gerenciador de tarefas
-      </h1>
-      <Tasks tasks={tasks} />
-      <AddTasks />
+      <div className="w-[500px] space-y-4">
+        <h1 className="text-3xl text-slate-100 font-bold text-center">
+          Gerenciador de tarefas
+        </h1>
+        <AddTasks 
+        onAddTaskSubmit={onAddTaskSubmit}
+        
+        />
+        <Tasks
+          tasks={tasks}
+          onTaskClick={onTaskClick}
+          onDeleteTaskClick={onDeleteTaskClick}
+        />
+      </div>
     </div>
   );
 }
